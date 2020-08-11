5065c7f47389edfb6b60ebb3a998365463ff3b68
irriver <maraqann@gmail.com>
2020-08-11 16:50:02 +0900

    캡슐화된 멤버 필드에 getter와 setter 메소드로 접근하기

---
+++ OOP_Basic/src/modifier/Car.java
@@ -0,0 +1,46 @@
+package modifier;
+
+/*
+ 	getter와 setter는 필요에 따라 있을 수도 없을수도
+ 	만약, private window = 10;으로 값을 고정해서 할당한다면
+ 	굳이 setter를 사용해 값을 할당할 필요는 없겠지
+ */
+
+public class Car {
+	private int window;
+	private int speed;
+
+	public int getWindow() {
+		return window;
+	}
+
+	public void setWindow(int window) {
+		this.window = window;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	// Quiz	>>	만약 speed에 음수가 들어오면 0 으로 강제 세팅
+	public void setSpeed(int speed) {
+		if (speed < 0) {
+			this.speed = -1;
+		} else {
+			this.speed = speed;
+		}
+	}
+	
+	//Quiz	>> 속도를 10씩 올리고 내리는 기능
+	public void upSpeed() {
+		this.speed += 10;
+	}
+
+	public void downSpeed() {
+		if (this.speed < 0) {
+			this.speed = 0;
+		} else {
+			this.speed -= 10;
+		}
+	}
+}
---
+++ OOP_Basic/src/modifier/ModifierTest.java
@@ -0,0 +1,33 @@
+package modifier;
+
+/*
+ 	접근자(한정자): modifier
+ 	public 공유: package 구분없이 공유
+ 	private 개인: 클래스 애부 공유자원, 참조변수 입장에서 접근 불가 사용(사용 불가)
+ 	
+ 	객체지향언어
+ 	1) 캡슐화(은닉화)
+ 		>>	클래스 내부에 있는 자원 (member field, member method) 적용
+ 			private int number:		직접 할당을 막고 간접할당을 통해서 자원을 보호	>>	간접 (함수 write: setter - read: getter)
+ 			private void call():	다른 함수의 보조(공통)	>>	클래스 내부에서 다른 함수가 호출해서 사용 
+ */
+public class ModifierTest {
+	public static void main(String[] args) {
+
+		Car c = new Car();
+		c.setSpeed(10);
+		c.setWindow(2);
+
+		System.out.println(c.getSpeed());
+		System.out.println(c.getWindow());
+
+		c.upSpeed();
+		c.upSpeed();
+		System.out.println("2번 up " + c.getSpeed());
+		
+		c.downSpeed();
+		c.downSpeed();
+		c.downSpeed();
+		System.out.println("1번 down " + c.getSpeed());
+	}
+}
