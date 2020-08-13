eab85dddf1b02da6d05cff62b5296111526663e5
irriver <maraqann@gmail.com>
2020-08-13 16:22:35 +0900

    기본 생성자와 매개변수를 받는 생성자

---
+++ OOP_Basic/src/constructors/Constructors.java
@@ -0,0 +1,33 @@
+//package constructors;
+//
+//class Car {
+//	String company;
+//	String color;
+//	String serialNo;
+//	int noOfEngine;
+//	
+//	//Constructor is called when the object is created
+//	//Default constructor
+//	Car () {}	
+//	//constructor can take parameters  which is used to initialize attributes -> can overloadin
+//	Car (String company, String color, String serialNo, int noOfEngine) {
+//		this.company = company;
+//		this.color = color;
+//		this.serialNo = serialNo;
+//		this.noOfEngine = noOfEngine;
+//	}
+//}
+//
+//public class Constructors {
+//	public static void main(String[] args) {
+//		CAR volvo = new CAR();
+//		CAR audi = new CAR("Audi", "Grey", "ad-1111", 2);
+//		
+//		System.out.println("[Volvo]'s Addr: " + volvo);
+//		System.out.println("--- Attributes of [Audi] ---");
+//		System.out.println(audi.company);
+//		System.out.println(audi.color);
+//		System.out.println(audi.serialNo);
+//		System.out.println(audi.noOfEngine);
+//	}
+//}
